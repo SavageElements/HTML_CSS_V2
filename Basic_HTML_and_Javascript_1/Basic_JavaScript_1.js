39bc5409eec9ecc2eb013cb908913e158ca6de83
J Rockwell Seebach <jrs@ironpinephoto.com>
2022-05-18 11:14:59 -0700

    recreated files HTML_1 & HTML_2 as well as Javascript_1 to get caught back up on new repositorY

---
+++ Basic_HTML_and_Javascript_1/Basic_JavaScript_1.js
@@ -0,0 +1,11 @@
+/* global document */
+
+alert("Hello, World!");
+
+function My_First_Function() {
+            var String = "Kiss Me, Im Irish!";
+            var result = String.fontcolor("green");
+            document.getElementById("IRISH").innerHTML =
+            result;
+        }
+        
